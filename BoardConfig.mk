20d68b70fc0daa5a22f36997e909161be64a3243
desalesouche <desalesouche@gmail.com>
2016-06-17 15:28:39 +0200

    disable odex cos it prevents rom to properly boot on Y550
    it seems system size might be little for odexed rom

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,11 +206,11 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Enable dex-preoptimization to speed up first boot sequence
-ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),userdebug)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-DONT_DEXPREOPT_PREBUILTS := true
+# ifeq ($(HOST_OS),linux)
+#   ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+#     ifeq ($(WITH_DEXPREOPT),)
+#       WITH_DEXPREOPT := true
+#     endif
+#   endif
+# endif
+# DONT_DEXPREOPT_PREBUILTS := true
